6770c4476a348c366fdbac679eedc130cedc3adf
bumrei <bumrei@gmail.com>
2021-07-09 17:25:39 +0900

    .

--- project/src/net/mini/project1/Handler.java
+++ project/src/net/mini/project1/Handler.java
@@ -1,5 +1,6 @@
 package net.mini.project1;
 
 public interface Handler {
-
+  public void execute();
+  public void menu();
 }
---
+++ project/src/net/mini/project1/ScoreCard.java
@@ -0,0 +1,37 @@
+package net.mini.project1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class ScoreCard {
+
+  public static void main(String[] args) {
+    Connection CN = null;  // DB 서버연결정보 - 서버 ip 주소 계정id, password
+    Statement ST = null;  // ST = CN.createStatement() 명령어 생성 - 신규등록해라, 조회하라, 삭제하라
+    ResultSet RS = null;  //select 조회결과값을 기억하는 덩어리 장치.
+    String msg = "isud=crud쿼리문 기술";
+    Scanner sc = new Scanner(System.in);
+
+    try {
+      Class.forName("oracle.jdbc.driver.OracleDriver"); // 오라클드라이브로드
+      String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE" ;
+      CN = DriverManager.getConnection(url , "system", "1234"); //오라클 서버, 계정, 비번, 연결하기. // XE는 우리가 설치한 DB서버의 이름. //CN은 getConnection의 리턴값이 Connection이니까 해주는것이다.
+      System.out.println("오라클 드라이브 및 서버연결 성공");
+
+      //첫번째명령어 생성
+      ST = CN.createStatement();
+
+      msg = "select score from member";
+      RS = ST.executeQuery(msg);
+
+      while ()
+
+
+    } catch (Exception ex) { System.out.println("에러 =" + ex);}
+
+  }
+}
+
