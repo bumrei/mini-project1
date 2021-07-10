91e05cf27c8e6e6de5e84a5876077e629146f93a
bumrei <bumrei@gmail.com>
2021-07-10 20:46:47 +0900

    login기능

--- project/src/net/mini/project1/Login.java
+++ project/src/net/mini/project1/Login.java
@@ -8,56 +8,57 @@ import java.util.Scanner;
 
 public class Login {
 
+  Connection CN = null;
+  Statement ST = null;
+  ResultSet RS = null;
+  String msg = null;
+  Scanner sc = new Scanner(System.in);
+  String uID = null;
+  String uPsw = null;
+  String userID = null;
+  String userPsw = null;
 
-  public static void main(String[] args) {
-    Connection CN = null;
-    Statement ST = null;
-    ResultSet RS = null;
-    String msg = null;
-    Scanner sc = new Scanner(System.in);
-    String uID = null;
-    String uPsw = null;
+  public static void main(String[] args) throws Exception {
+    Login lg = new Login();
+    lg.dbConnect();
+    lg.loginFrame();
+    lg.selectFromDB();
+    lg.matchWithDB();
+  }//main END
 
-    try {
-      Class.forName("oracle.jdbc.driver.OracleDriver");
-      String url = "jdbc:oracle:thin:@localhost:1521:XE";
-      CN = DriverManager.getConnection(url, "system", "1234");
-      System.out.println("연결 성공");
+  public void dbConnect() throws Exception {
+    Class.forName("oracle.jdbc.driver.OracleDriver");
+    String url = "jdbc:oracle:thin:@localhost:1521:XE";
+    CN = DriverManager.getConnection(url, "system", "1234");
+    System.out.println("연결 성공");
+    ST = CN.createStatement(); 
+  }// dbConnect End
 
-      ST = CN.createStatement(); // CN에 접속해 명령어를 생성하고 실행 대기하라.
+  public void loginFrame() {
+    System.out.print("ID >> ");
+    userID = sc.nextLine();
 
-      // 로그인 창
-      System.out.print("ID >> ");
-      String userID = sc.nextLine();
+    System.out.print("Password >> ");
+    userPsw = sc.nextLine();
+  }// loginFrame End
 
-      System.out.println("Password >> ");
-      String userPsw = sc.nextLine();
+  public void selectFromDB() throws Exception {
+    msg = "select ID, PSW from member where ID = '" + userID + "'";
+    RS = ST.executeQuery(msg);
+    if (RS.next()==true) {
+      uID = RS.getString("ID");
+      uPsw = RS.getString("PSW");
+    }
+  }// selectFromDB End
 
-      // 사용자 정보 DB에서 select 하기
-
-      msg = "select ID, PSW from member where ID = '" + userID + "'";
-      RS = ST.executeQuery(msg); // CN에 접속해 RS에 select 명령어를 생성하고 실행해라.
-      if (RS.next()==true) {
-        uID = RS.getString("ID");
-        uPsw = RS.getString("PSW");
-      }
-
-      // DB 사용자 정보와 입력정보 메치 확인하기.
-      if (userID.equals(uID) && userPsw.equals(uPsw)) {
-        System.out.println("로그인에 성공하였습니다.");
-        // 여기에 게임실행 메서드가 들어갈 것이다.
-
-      } else {System.out.println("아이디 혹은 비밀번호가 일치하지 않습니다.");}
-
-
-
-
-
-
-
-
-    }catch (Exception ex) {System.out.println("에러 이유 =" + ex); }
-
-  }
-
-}
+  public void matchWithDB() throws Exception {
+    if (userID.equals(uID) && userPsw.equals(uPsw)) {
+      System.out.println("로그인에 성공하였습니다.");
+      System.out.println("게임 로딩중...");
+      Thread.sleep(2000);
+      System.out.println("범래: 그다음 게임이 시작됩니다. 여기에 게임 실행 메서드가 들어갈겁니다.");
+      System.out.println("모두 화이팅");
+      // 여기에 게임실행 메서드가 들어갈 것이다.
+    } else {System.out.println("아이디 혹은 비밀번호가 일치하지 않습니다.");}
+  }// matchWithDB End
+}//Class End
