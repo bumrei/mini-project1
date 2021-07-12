4c5699bf102e1a38fda5d2c1c9f174153a48fe42
bumrei <bumrei@gmail.com>
2021-07-12 12:42:48 +0900

    .

--- project/src/net/mini/project1/SqlDb.java
+++ project/src/net/mini/project1/SqlDb.java
@@ -1,6 +1,10 @@
 package net.mini.project1;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class SqlDb {
@@ -10,8 +14,8 @@ public class SqlDb {
   ResultSet RS = null; //select조회결과값 전체데이터를 기억
   PreparedStatement pst = null;
   String sql = "select * from word";
- Scanner sc = new Scanner(System.in);
-  
+  Scanner sc = new Scanner(System.in);
+
 
   public void dbConnect() {
     try {
@@ -21,14 +25,14 @@ public class SqlDb {
       System.out.println("오라클 드라이브 및 서버연결성공");
       ST = CN.createStatement();
       RS = ST.executeQuery(sql);
-     }catch(Exception ex) { System.out.println("에러이유 " + ex);}
+    }catch(Exception ex) { System.out.println("에러이유 " + ex);}
   }//dbconnect end
 
-   
-  public Boolean select(String id, String psw) throws Exception {
+
+  public boolean select(String id, String psw) throws Exception {
     String uid = null, upsw = null ;
     sql = "select * from member where ID = ? ";
-   // RS = ST.executeQuery(sql);
+    // RS = ST.executeQuery(sql);
     pst = CN.prepareStatement(sql);
     pst.setString( 1 , id );
     RS = pst.executeQuery();
@@ -37,17 +41,17 @@ public class SqlDb {
       upsw = RS.getString("PSW");
     } 
     if (id.equals(uid) && psw.equals(upsw)) {
-     return true ;
+      return true ;
     } else { return false ;}
   }// select End 
-  
-  
+
+
 
   public boolean delete(String id) throws Exception {
     sql = "delect from member where ID = ? ";
     pst = CN.prepareStatement(sql);
     pst.setString( 1 , id );
-    
+
     String delId = null;
     if (RS.next()) {
       delId = RS.getString("ID");
@@ -55,19 +59,19 @@ public class SqlDb {
     if (id.equals(delId) ) {
       System.out.println("정말 삭제하시겠습니까? (y/N)");
       String command = sc.nextLine();
-         if (command.equals("y")) {
-           pst.executeUpdate();
-            return true ;
-         } else {
-           System.out.println("삭제를 취소합니다.\n");
-           return false ;
-           }
-     } else { return false ; }
+      if (command.equals("y")) {
+        pst.executeUpdate();
+        return true ;
+      } else {
+        System.out.println("삭제를 취소합니다.\n");
+        return false ;
+      }
+    } else { return false ; }
 
   }//Delete 
-  
-  
 
 
- 
+
+
+
 }
