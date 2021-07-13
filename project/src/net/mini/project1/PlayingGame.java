a4fe9a0a544aa1c38babec434f6c07c5c26ba729
hyunn12 <w3whhhh@gmail.com>
2021-07-13 16:28:48 +0900

    단어출력추가

--- project/src/net/mini/project1/PlayingGame.java
+++ project/src/net/mini/project1/PlayingGame.java
@@ -1,10 +1,38 @@
 package net.mini.project1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class PlayingGame {
-
+  Connection CN = null;
+  Statement ST = null;
+  ResultSet RS = null;
+  PreparedStatement pstmt = null;
   Scanner sc = new Scanner(System.in);
+  int sLevel = 0;
+  String sql = null;
+
+  public void dbConnect() throws Exception {
+    Class.forName("oracle.jdbc.driver.OracleDriver");
+    String url = "jdbc:oracle:thin:@localhost:1521:XE";
+    CN = DriverManager.getConnection(url, "system", "1234");
+    ST = CN.createStatement(); 
+  }// dbConnect End
+
+  public static void main(String[] args) {
+    try {
+      PlayingGame pg = new PlayingGame();
+
+      pg.dbConnect();
+      pg.game();
+
+    } catch(Exception e) {}
+  }
+
 
   public void game() throws Exception {
 
@@ -19,9 +47,7 @@ public class PlayingGame {
       String command = sc.nextLine();
 
       switch (command) {
-        case "1": // 단어 암기 (깜박이)
-          System.out.println("준비중");
-          break;
+        case "1": this.selectLevel(); break;
         case "2": // 단어 테스트 게임
           System.out.println("준비중");
           break;
@@ -41,8 +67,49 @@ public class PlayingGame {
           System.out.println("게임을 종료합니다.");
           System.exit(0);
         default :
-          System.out.println("옳바른 번호를 입력해 주세요.  !!!( •̀ ᴗ •́ )و!!!");
+          System.out.println("올바른 번호를 입력해 주세요.  !!!( •̀ ᴗ •́ )و!!!");
       }
+    }
+  }
+
+
+
+  public void selectLevel() throws Exception {
+    Loop :while(true) {
+      System.out.print("\nLevel 선택 [1~3] [8. 뒤로가기]\n >>> ");
+      try {
+        sLevel = Integer.parseInt(sc.nextLine());
+
+        if (sLevel >= 1 && sLevel <=3) {
+          System.out.println("\n  Level: " + sLevel);
+
+          System.out.print("\n출력할 단어 시작번호>>> ");
+          int snum = Integer.parseInt(sc.nextLine());
+          System.out.print("출력할 단어 마지막번호>>> ");
+          int fnum = Integer.parseInt(sc.nextLine());
+
+          sql = "select * from (select row_number() "
+              + "over(partition by wordlevel order by wordNum) as row_num, b.* from word b) "
+              + "a where wordlevel = " + sLevel + " and a.row_num between ? and ? ";
+          pstmt = CN.prepareStatement(sql);
+          pstmt.setInt(1, snum);
+          pstmt.setInt(2, fnum);
+          RS = pstmt.executeQuery();
+
+          System.out.println("\n  영단어 \t 단어뜻");
+          System.out.println("---------------------------");
+          while(RS.next() == true) {
+            String peng = RS.getString("eng");
+            String pkor = RS.getString("kor");
+            System.out.println("  " + peng + " \t " + pkor);
+          }
+        } else if (sLevel == 8) {
+          System.out.println("뒤로가기");
+          break Loop;
+        } else {
+          System.out.println("\n번호 다시 확인해주세요.");
+        }
+      } catch(Exception e) {System.out.println("번호 다시 확인해주세요");}
     }
   }
 
